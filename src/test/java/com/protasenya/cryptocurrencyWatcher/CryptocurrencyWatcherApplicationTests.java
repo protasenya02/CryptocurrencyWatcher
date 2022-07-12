112bc8f7f2987d648f77bfa699574536f57fb86b
Valentine Protasenya <protasenya02@icloud.com>
2022-07-12 11:25:51 +0300

    Create Spring Boot application

---
+++ src/test/java/com/protasenya/cryptocurrencyWatcher/CryptocurrencyWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protasenya.cryptocurrencyWatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrencyWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
